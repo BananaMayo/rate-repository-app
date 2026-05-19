991744f945b580060c58b87e4112e6fe291aab0e
BananaMayo <niklas.nygard01@gmail.com>
2026-05-19 14:27:00 +0300

    Exercise 10.20

--- src/components/RepositoryInfo.jsx
+++ src/components/RepositoryInfo.jsx
@@ -1,6 +1,65 @@
+import { FlatList, View, StyleSheet } from 'react-native';
+
 import { useParams } from 'react-router-native';
+import { format } from 'date-fns';
+
 import RepositoryItem from './RepositoryItem';
+import Text from './Text';
+import theme from '../theme';
 import useRepository from '../hooks/useRepository';
+
+const styles = StyleSheet.create({
+  separator: {
+    height: 10,
+    backgroundColor: theme.colors.mainBackground,
+  },
+  reviewContainer: {
+    backgroundColor: 'white',
+    padding: 15,
+    flexDirection: 'row',
+  },
+  ratingContainer: {
+    width: 50,
+    height: 50,
+    borderRadius: 25,
+    borderWidth: 2,
+    borderColor: theme.colors.primary,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginRight: 15,
+  },
+  ratingText: {
+    color: theme.colors.primary,
+  },
+  reviewContent: {
+    flex: 1,
+  },
+  date: {
+    marginBottom: 10,
+  },
+});
+
+const ItemSeparator = () => <View style={styles.separator} />;
+
+const ReviewItem = ({ review }) => {
+  return (
+    <View style={styles.reviewContainer}>
+      <View style={styles.ratingContainer}>
+        <Text fontWeight="bold" style={styles.ratingText}>
+          {review.rating}
+        </Text>
+      </View>
+
+      <View style={styles.reviewContent}>
+        <Text fontWeight="bold">{review.user.username}</Text>
+        <Text color="textSecondary" style={styles.date}>
+          {format(new Date(review.createdAt), 'dd MMM yyyy')}
+        </Text>
+        <Text>{review.text}</Text>
+      </View>
+    </View>
+  );
+};
 
 const RepositoryInfo = () => {
   const { id } = useParams();
@@ -10,7 +69,21 @@ const RepositoryInfo = () => {
     return null;
   }
 
-  return <RepositoryItem item={repository} showGitHubButton />;
+  const reviews = repository.reviews
+    ? repository.reviews.edges.map(edge => edge.node)
+    : [];
+
+  return (
+    <FlatList
+      data={reviews}
+      renderItem={({ item }) => <ReviewItem review={item} />}
+      keyExtractor={({ id }) => id}
+      ItemSeparatorComponent={ItemSeparator}
+      ListHeaderComponent={() => (
+        <RepositoryItem item={repository} showGitHubButton />
+      )}
+    />
+  );
 };
 
 export default RepositoryInfo;
